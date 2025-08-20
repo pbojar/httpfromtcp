61c591a48f616031088b9df71cbc4cf0bf72e59e
pbojar <preston.bowesjaros@gmail.com>
2025-08-20 16:29:32 -0400

    Ch5L2 - complete

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -16,14 +17,14 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, nil
 	}
 	if idx == 0 {
-		return 0, true, nil
+		return 2, true, nil
 	}
 	headerLineText := string(data[:idx])
 	key, value, err := kvPairFromString(headerLineText)
 	if err != nil {
 		return 0, false, err
 	}
-	h[key] = value
+	h.Set(key, value)
 	return idx + 2, false, nil
 }
 
@@ -33,6 +34,37 @@ func kvPairFromString(headerString string) (key, value string, err error) {
 		return "", "", fmt.Errorf("error: invalid header string")
 	}
 	key = strings.Trim(parts[0], ":")
+	if !validTokens([]byte(key)) {
+		return "", "", fmt.Errorf("error: invalid header token found: %s", key)
+	}
 	value = parts[1]
 	return key, value, nil
+}
+
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	h[key] = value
+}
+
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
+
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if !isTokenChar(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isTokenChar(c byte) bool {
+	if c >= 'A' && c <= 'Z' ||
+		c >= 'a' && c <= 'z' ||
+		c >= '0' && c <= '9' {
+		return true
+	}
+
+	return slices.Contains(tokenChars, c)
 }
